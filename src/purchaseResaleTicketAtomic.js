4cd5c5596bbf21f8631eef41c8ad1af4fbe1f1cb
suyash-2004 <suyash096@gmail.com>
2025-10-26 20:51:25 +0530

    updated listTicketForSaleAtomic.js

--- src/purchaseResaleTicketAtomic.js
+++ src/purchaseResaleTicketAtomic.js
@@ -225,9 +225,6 @@ export default async ({ req, res, log, error }) => {
       quantity: quantityPurchased,
       isListedForSale: 'false',
       qrCodeFileId: `${newTicketId}_ticket_qr.png`,
-      purchaseType: 'resale',
-      originalTicketId: originalTicketId,
-      sellerUserId: sellerUserId
     };
 
     await databases.createDocument(
